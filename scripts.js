f3fd4009987ea984e5c545fc76bda097ecfaed38
DanGrund <hello@dangrund.com>
2016-10-27 12:34:36 -0600

    refactor javascript and css

--- scripts.js
+++ scripts.js
@@ -2,16 +2,37 @@ var ideaArray = [];
 var sortedArray = [];
 var $title = $('#title');
 var $body = $('#body');
+var sortOrder = false;
 var qualityChangers = {
   up: {genius: "genius", plausible: "genius", swill: "plausible"},
   down: {genius: "plausible", plausible: "swill", swill: "swill"}
 };
 
 $('document').ready( function() {
-  var holdingValue = JSON.parse(localStorage.getItem("ideaArray"));
-  if (holdingValue){
-    ideaArray = holdingValue;
-    render(ideaArray);
+  loadPage();
+});
+
+$("#title, #body").keyup(function(){
+  checkField();
+});
+
+$('#save').on('click', function(e){
+  var title = $title.val();
+  var body = $body.val();
+  var idea = new Idea(title, body);
+  storeIdea(idea);
+  createCard(idea);
+  $title.val("");
+  $body.val("");
+});
+
+$('#sort').on('click', function(e) {
+  if (!sortOrder) {
+    render(downSort());
+    sortOrder = !sortOrder;
+  } else {
+    render(upSort());
+    sortOrder = !sortOrder;
   }
 });
 
@@ -24,73 +45,11 @@ $('#search').on('keyup', function(e) {
   return render();
 });
 
-function render(givenArray) {
-  if (givenArray) renderArray = givenArray;
-  if (!givenArray) renderArray = ideaArray;
-  $('#ideas').empty();
-  for (var i = 0; i < renderArray.length; i++) {
-  createCard(renderArray[i]);
-  }
-}
-
-$('#save').on('click', function(e){
-  var title = $title.val();
-  var body = $body.val();
-  var idea = new Idea(title, body);
-  storeIdea(idea);
-  createCard(idea);
-  $title.val("");
-  $body.val("");
-});
-
 $("#ideas").on("click", "#delete-btn", function(){
   $(this).closest("article").remove();
   var id = this.closest("article").id;
   deleteIdea(id);
 });
-
-function deleteIdea (id) {
-  for (var i = 0; i < ideaArray.length; i++) {
-    var ideaId = ideaArray[i].id;
-    if (id == ideaId) ideaArray.splice(i, 1);
-    localStorage.setItem("ideaArray",JSON.stringify(ideaArray));
-  }
-}
-
-function Idea(title, body) {
-  this.id = new Date().getTime();
-  this.title = title;
-  this.body = body;
-  this.quality = 'swill';
-}
-
-function storeIdea (idea) {
-  ideaArray.push(idea);
-  localStorage.setItem("ideaArray", JSON.stringify(ideaArray));
-}
-
-function createCard(idea) {
-  $('#ideas').prepend(`<article class="newIdea" id=${idea.id}>
-    <div id = "card-top">
-      <h1 id="idea-title" contenteditable>${idea.title}</h1>
-      <button id="delete-btn"></button>
-    </div>
-    <div id = "card-middle">
-      <p id="idea-body" contenteditable>${idea.body}</p>
-    </div>
-    <div id = "card-bottom">
-      <button id="up-btn"></button>
-      <button id="down-btn"></button>
-      <h2 id="quality">quality: ${idea.quality}</h2>
-    </div>
-  </article>`);
-}
-
-function findIdeaByID(id) {
-  return ideaArray.filter(function(idea) {
-    return idea.id === id;
-  })[0];
-}
 
 $("#ideas").on('click', "#up-btn", function(){
   var id = +$(this).closest("article").attr('id');
@@ -114,11 +73,9 @@ $("#ideas").on('click', "#down-btn", function(){
       idea.quality = qualityChangers.down[ideaQuality];
     }
   });
-
   $(this).siblings("h2").text("quality: "+qualityChangers.down[ideaQuality]);
   localStorage.setItem("ideaArray", JSON.stringify(ideaArray));
 });
-
 
 $('#ideas').on('keyup blur', "#idea-title", function(e) {
   if (e.which == 13 || e.type === "focusout") {
@@ -152,27 +109,24 @@ $('#ideas').on('keyup blur', "#idea-body", function(e) {
   }
 });
 
-$('#sort').on('click', function(e) {
-  if (!sortOrder) {
-    render(downSort())
-    sortOrder = !sortOrder
-  } else {
-    render(upSort())
-    sortOrder = !sortOrder
+function loadPage() {
+  var holdingValue = JSON.parse(localStorage.getItem("ideaArray"));
+  if (holdingValue){
+    ideaArray = holdingValue;
+    render(ideaArray);
   }
-});
-
-var sortOrder = false;
-
-function upSort() {
-  return ideaArray.sort(function(a, b) { return a.quality > b.quality })
 }
 
-function downSort() {
-  return ideaArray.sort(function(a, b) { return a.quality < b.quality })
+function render(givenArray) {
+  if (givenArray) renderArray = givenArray;
+  if (!givenArray) renderArray = ideaArray;
+  $('#ideas').empty();
+  for (var i = 0; i < renderArray.length; i++) {
+  createCard(renderArray[i]);
+  }
 }
 
-$("#title, #body").keyup(function(){
+function checkField() {
   var checkTitle = /\S/.test($("#title").val());
   var checkBody = /\S/.test($("#body").val());
   if(checkTitle && checkBody){
@@ -180,4 +134,55 @@ $("#title, #body").keyup(function(){
   } else {
     $("#save").attr("disabled", true);
   }
-});
+}
+
+function Idea(title, body) {
+  this.id = new Date().getTime();
+  this.title = title;
+  this.body = body;
+  this.quality = 'swill';
+}
+
+function storeIdea (idea) {
+  ideaArray.push(idea);
+  localStorage.setItem("ideaArray", JSON.stringify(ideaArray));
+}
+
+function deleteIdea (id) {
+  for (var i = 0; i < ideaArray.length; i++) {
+    var ideaId = ideaArray[i].id;
+    if (id == ideaId) ideaArray.splice(i, 1);
+    localStorage.setItem("ideaArray",JSON.stringify(ideaArray));
+  }
+}
+
+function createCard(idea) {
+  $('#ideas').prepend(`<article class="newIdea" id=${idea.id}>
+    <div id = "card-top">
+      <h1 id="idea-title" contenteditable>${idea.title}</h1>
+      <button id="delete-btn"></button>
+    </div>
+    <div id = "card-middle">
+      <p id="idea-body" contenteditable>${idea.body}</p>
+    </div>
+    <div id = "card-bottom">
+      <button id="up-btn"></button>
+      <button id="down-btn"></button>
+      <h2 id="quality">quality: ${idea.quality}</h2>
+    </div>
+  </article>`);
+}
+
+function findIdeaByID(id) {
+  return ideaArray.filter(function(idea) {
+    return idea.id === id;
+  })[0];
+}
+
+function upSort() {
+  return ideaArray.sort(function(a, b) { return a.quality > b.quality })
+}
+
+function downSort() {
+  return ideaArray.sort(function(a, b) { return a.quality < b.quality });
+}
